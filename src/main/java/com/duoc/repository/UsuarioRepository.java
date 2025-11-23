8b82a16d5923636be572ec5910b891d45f459393
jai-valencia <jai.valencia@duocuc.cl>
2025-11-23 18:51:08 -0300

    Creación de los repositorios del backend

---
+++ src/main/java/com/duoc/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.duoc.repository;
+
+import com.duoc.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByRut(String rut);
+}
+
